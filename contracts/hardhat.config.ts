46b3aa67a5c19aa6b2473301c7641d41915f2873
Josh <josh_alot@protonmail.com>
2026-01-07 23:43:41 -0300

    setting up dotenv

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,8 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable, defineConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
